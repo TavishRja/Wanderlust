293bbf08090fc6e8bae4d33263d53661d00127a3
Tavish Rja <tavishrja786@gmail.com>
2025-12-15 16:13:05 +0530

    Fix listing owner null issue by populating owner and adding EJS safety check

--- init/index.js
+++ init/index.js
@@ -1,17 +1,11 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = "mongodb+srv://tavish:VpeW1FrUNF1ci1pF@cluster0.t7mza.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  .then(() => console.log("connected to DB"))
+  .catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(dbUrl);
@@ -19,7 +13,14 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data =initData.data.map((obj)=> ({...obj, owner:"652d0081ae547c5d37e56b5f"}));
+
+  const ownerId = new mongoose.Types.ObjectId("652d0081ae547c5d37e56b5f");
+
+  initData.data = initData.data.map(obj => ({
+    ...obj,
+    owner: ownerId
+  }));
+
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
